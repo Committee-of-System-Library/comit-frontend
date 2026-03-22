c049a8d5c4abcddf7aa6cf9083d7f39864ecf0eb
nyeong <hannayoung1210@gmail.com>
2026-03-22 22:26:16 +0900

    feat: AdminPostCard 컴포넌트 구현 & types/post.ts 추가 & utils/formatTime.ts 추가

--- src/shared/ui/WritingButton.tsx
+++ src/shared/ui/WritingButton.tsx
@@ -9,10 +9,10 @@ export const WritingButton = ({ disabled = false }: WritingButtonProps) => {
   return (
     <button
       disabled={disabled}
-      className="group w-full py-3.25 flex items-center justify-center gap-2 rounded-xl bg-primary-600 hover:bg-primary-1000 transition-colors duration-200 disabled:opacity-50 disabled:cursor-not-allowed"
+      className="group w-full h-12 py-3.25 flex items-center justify-center gap-2 rounded-xl bg-primary-600 hover:bg-primary-1000 transition-colors duration-200 disabled:opacity-50 disabled:cursor-not-allowed"
     >
       <PencilLine className="w-5 h-5 text-text-inverse group-hover:text-text-deactivated transition-colors duration-200" />
-      <p className=" text-label-01 text-text-inverse group-hover:text-text-deactivated transition-colors duration-200">
+      <p className="text-label-01 text-text-inverse group-hover:text-text-deactivated transition-colors duration-200">
         글 작성하기
       </p>
     </button>
