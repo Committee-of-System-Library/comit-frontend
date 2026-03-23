392032e2aa30100f06b55163c21ed29a2f3f0842
Junsub Lee <lee980605@knu.ac.kr>
2026-03-23 23:28:05 +0900

    feat: AppDesktopShell 도입 및 공통 데스크톱 레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { AppDesktopShell } from "@/app/layout/AppDesktopShell";
 import HomePage from "@/pages/home/HomePage";
 
 function App() {
-  return <HomePage />;
+  return (
+    <AppDesktopShell>
+      <HomePage />
+    </AppDesktopShell>
+  );
 }
 
 export default App;
