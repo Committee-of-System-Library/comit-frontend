213ee7cff26309a7b4eb77f81eec3b5148ea6274
nyeong <hannayoung1210@gmail.com>
2026-03-24 16:46:24 +0900

    refactor: 상세페이지 컴포넌트 & stories 파일 폴더 추가 및 정리

---
+++ src/shared/ui/AdminPostCard/AdminPostCard.tsx
@@ -0,0 +1,57 @@
+import { UserRound, Heart, MessageCircleMore } from "lucide-react";
+
+import type { Post } from "@/types/post";
+import { cn } from "@/utils/cn";
+import { formatTimeAgo } from "@/utils/formatTime";
+
+interface AdminPostCardProps extends Post {
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+export const AdminPostCard = ({
+  title,
+  content,
+  user,
+  heart,
+  comment,
+  createdAt,
+  disabled = false,
+}: AdminPostCardProps) => {
+  return (
+    <article
+      className={cn(
+        "w-full px-4 py-3.5 flex items-start justify-between rounded-xl border border-border-deactivated bg-background-light hover:bg-background-dark transition-colors duration-200",
+        disabled && "opacity-50 pointer-events-none",
+      )}
+    >
+      <div className="flex flex-1 flex-col gap-2 overflow-hidden pr-4">
+        <div className="flex flex-col gap-1">
+          <header className="text-subtitle-03 text-text-primary line-clamp-1">
+            {title}
+          </header>
+          <p className="text-body-02 text-text-tertiary line-clamp-2">
+            {content}
+          </p>
+        </div>
+        <div className="flex gap-2 text-text-placeholder text-caption-02">
+          <div className="flex items-center gap-1">
+            <UserRound className="w-4 h-4" />
+            <span className="leading-none">{user}</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <Heart className="w-4 h-4" />
+            <span className="leading-none">{heart}</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <MessageCircleMore className="w-4 h-4" />
+            <span className="leading-none">{comment}</span>
+          </div>
+        </div>
+      </div>
+      <p className="text-caption-02 text-text-placeholder shrink-0">
+        {formatTimeAgo(createdAt)}
+      </p>
+    </article>
+  );
+};
---
+++ src/shared/ui/WrtingButton/WritingButton.tsx
@@ -0,0 +1,21 @@
+import { PencilLine } from "lucide-react";
+
+interface WritingButtonProps {
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+export const WritingButton = ({ disabled = false }: WritingButtonProps) => {
+  return (
+    <button
+      disabled={disabled}
+      className="group w-full h-12 py-3.25 flex items-center justify-center gap-2 rounded-xl bg-primary-600 hover:bg-primary-1000 transition-colors duration-200 disabled:opacity-50 disabled:cursor-not-allowed"
+      aria-label="글 작성하기 버튼"
+    >
+      <PencilLine className="w-5 h-5 text-text-inverse group-hover:text-text-deactivated transition-colors duration-200" />
+      <p className="text-label-01 text-text-inverse group-hover:text-text-deactivated transition-colors duration-200">
+        글 작성하기
+      </p>
+    </button>
+  );
+};
