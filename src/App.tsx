0c45ba9d1dcee379ac1f374f190e6411c8a88370
nyeong <hannayoung1210@gmail.com>
2026-03-24 18:30:09 +0900

    feat: App.tsx 페이지 라우팅 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
 import { AppDesktopShell } from "@/app/layout/AppDesktopShell";
+import EventBoardPage from "@/pages/board/EventBoardPage";
+import FreeBoardPage from "@/pages/board/FreeBoardPage";
+import InfoBoardPage from "@/pages/board/InfoBoardPage";
+import NoticeBoardPage from "@/pages/board/NoticeBoardPage";
+import QnABoardPage from "@/pages/board/QnABoardPage";
 import HomePage from "@/pages/home/HomePage";
 
 function App() {
   return (
     <AppDesktopShell>
-      <HomePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/qna" element={<QnABoardPage />} />
+          <Route path="/info" element={<InfoBoardPage />} />
+          <Route path="/free" element={<FreeBoardPage />} />
+          <Route path="/notice" element={<NoticeBoardPage />} />
+          <Route path="/event" element={<EventBoardPage />} />
+        </Routes>
+      </BrowserRouter>
     </AppDesktopShell>
   );
 }
