e0729a09be77be8f6de86bcf586a83fd8b2302db
nyeong <hannayoung1210@gmail.com>
2026-03-28 23:23:40 +0900

    docs: ReportModal 스토리 작성

---
+++ src/shared/ui/ReportModal/ReportModal.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { fn } from "@storybook/test";
+
+import { ReportModal } from "./ReportModal";
+
+const meta: Meta<typeof ReportModal> = {
+  title: "Shared/UI/ReportModal",
+  component: ReportModal,
+  tags: ["autodocs"],
+  decorators: [
+    (Story) => {
+      if (!document.getElementById("modal-portal")) {
+        const portalRoot = document.createElement("div");
+        portalRoot.setAttribute("id", "modal-portal");
+        document.body.appendChild(portalRoot);
+      }
+      return <Story />;
+    },
+  ],
+  parameters: {
+    docs: {
+      description: {
+        component: "신고 모달창입니다. 신고 사유 최대 30자까지 입력 가능",
+      },
+    },
+  },
+  args: {
+    disabled: false,
+    user: "냠냠",
+    contents: "야식메뉴 추천받을게요. 어제 마라샹궈랑 계란초밥 먹음",
+    onClose: fn(() => console.info("모달 닫기")),
+    onConfirm: fn((reason) => console.info("신고 접수 사유:", reason)),
+  },
+} satisfies Meta<typeof ReportModal>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
