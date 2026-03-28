40ffe1c832cd4e4ce98f75cad6ff207872b320a8
nyeong <hannayoung1210@gmail.com>
2026-03-29 05:23:44 +0900

    feat: react-hot-toast 라이브러리 설치 & App.tsx 에 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 
 import { AuthStateDevTool } from "@/app/devtools/AuthStateDevTool";
@@ -75,6 +76,26 @@ function App() {
 
   return (
     <>
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          className:
+            "!bg-gray-800 !text-white !rounded-xl !px-4 !py-2 !text-caption-02 !shadow-md",
+          success: {
+            iconTheme: {
+              primary: "#30D158",
+              secondary: "#FFFFFF",
+            },
+          },
+          error: {
+            iconTheme: {
+              primary: "#FF4245",
+              secondary: "#FFFFFF",
+            },
+          },
+          duration: 3000,
+        }}
+      />
       <BrowserRouter>
         <AppContent isAuthenticated={isAuthenticated} />
       </BrowserRouter>
